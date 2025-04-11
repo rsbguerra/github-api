06adec2060e8f04729484b2357fdeecd97551073
rsbguerra <rsbguerra@tutamail.com>
2025-04-11 12:30:24 +0100

    Refactor Repository struct to RepositoryModel
    - Model now extends github.Repositories

--- pkg/models/repository.go
+++ pkg/models/repository.go
@@ -1,24 +1,21 @@
 package models
 
 import (
-    "context"
-    "github.com/gin-gonic/gin"
-    "github.com/go-git/go-git/v5"
-    "github.com/google/go-github/v50/github"
-    "os"
+	"context"
+	"github.com/gin-gonic/gin"
+	"github.com/go-git/go-git/v5"
+	"github.com/google/go-github/v50/github"
+	"os"
 )
 
-// Repository represents a GitHub repository with its basic details.
+// RepositoryModel represents a GitHub repository with its basic details.
 // It includes the repository's name, URL, owner, and privacy status.
-type Repository struct {
-    Name    string `json:"name"`
-    URL     string `json:"url"`
-    Owner   string `json:"owner"`
-    Private bool   `json:"private"`
+type RepositoryModel struct {
+	*github.Repository
 }
 
-// NewRepository creates a new Repository instance by binding JSON data from the provided
-// gin.Context. It parses the incoming JSON payload into a Repository struct.
+// ConvertFromContext creates a new RepositoryModel instance by binding JSON data from the provided
+// gin.Context. It parses the incoming JSON payload into a RepositoryModel struct.
 //
 // Parameters:
 //   - c: The gin.Context containing the JSON payload to be bound.
@@ -26,13 +23,13 @@ type Repository struct {
 // Returns:
 //   - Repository: The populated Repository struct.
 //   - error: An error if the JSON binding fails, otherwise nil.
-func NewRepository(c *gin.Context) (Repository, error) {
-    var repo Repository
+func ConvertFromContext(c *gin.Context) (RepositoryModel, error) {
+	var repo RepositoryModel
 
-    if err := c.ShouldBindJSON(&repo); err != nil {
-        return Repository{}, err
-    }
-    return repo, nil
+	if err := c.ShouldBindJSON(&repo); err != nil {
+		return RepositoryModel{}, err
+	}
+	return repo, nil
 }
 
 // CloneRepo clones the repository specified by the Repository struct to a local directory.
@@ -41,16 +38,16 @@ func NewRepository(c *gin.Context) (Repository, error) {
 //
 // Returns:
 //   - error: An error if the cloning process fails, otherwise nil.
-func (r *Repository) CloneRepo() error {
-    _, err := git.PlainClone(r.Name, false, &git.CloneOptions{
-        URL:      r.URL,
-        Progress: os.Stdout,
-    })
-    return err
+func (r *RepositoryModel) CloneRepo() error {
+	_, err := git.PlainClone(*r.Name, false, &git.CloneOptions{
+		URL:      *r.URL,
+		Progress: os.Stdout,
+	})
+	return err
 }
 
 // CreateNew creates a new GitHub repository using the provided GitHub client.
-// It initializes a repository object with the current Repository struct's
+// It initializes a repository object with the current Repository structs
 // Name and Private fields, and then sends a request to create the repository
 // in the authenticated user's account.
 //
@@ -59,15 +56,15 @@ func (r *Repository) CloneRepo() error {
 //
 // Returns:
 //   - An error if the repository creation fails, otherwise nil.
-func (r *Repository) CreateNew(client *github.Client) error {
+func (r *RepositoryModel) CreateNew(client *github.Client) error {
 
-    repo := &github.Repository{
-        Name:    github.String(r.Name),
-        Private: github.Bool(r.Private),
-    }
+	repo := &github.Repository{
+		Name:    github.String(*r.Name),
+		Private: github.Bool(*r.Private),
+	}
 
-    _, _, err := client.Repositories.Create(context.Background(), "", repo)
-    return err
+	_, _, err := client.Repositories.Create(context.Background(), "", repo)
+	return err
 }
 
 // DeleteRepo deletes the repository associated with the Repository struct
@@ -76,16 +73,12 @@ func (r *Repository) CreateNew(client *github.Client) error {
 //
 // Parameters:
 //   - client: A pointer to a github.Client instance used to interact with
-//             the GitHub API.
+//     the GitHub API.
 //
 // Returns:
 //   - error: An error if the deletion fails, or nil if the operation is
-//            successful.
-func (r *Repository) DeleteRepo(client *github.Client) error {
-    _, err := client.Repositories.Delete(context.Background(), r.Owner, r.Name)
-    return err
+//     successful.
+func (r *RepositoryModel) DeleteRepo(client *github.Client) error {
+	_, err := client.Repositories.Delete(context.Background(), *r.Owner.Name, *r.Name)
+	return err
 }
-
-//func (r *Repository) ListPullRequests(client *github.Client) (*github.Repository, error) {
-// TODO
-//}
