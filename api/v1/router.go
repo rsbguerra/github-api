ee03e1e28320b622af43b5470a277a1c0289e370
rsbguerra <rsbguerra@tutamail.com>
2025-04-13 22:10:24 +0100

    Add missing token parameter

--- api/v1/router.go
+++ api/v1/router.go
@@ -1,14 +1,14 @@
 package v1
 
 import (
-	"github-api/internal/controllers"
+	controllers2 "github-api/pkg/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRoutes(router *gin.Engine) {
-	router.POST("/repositories/:token", controllers.CreateRepo)
-	router.DELETE("/repositories/:token", controllers.DeleteRepo)
-	router.GET("/users/:username/repositories", controllers.ListRepos)
-	router.GET("/repositories/:username/:repoName/pull-requests", controllers.PullRequests)
-	router.GET("/", controllers.Index)
+	router.POST("/repositories/:token", controllers2.CreateRepo)
+	router.DELETE("/repositories/:token", controllers2.DeleteRepo)
+	router.GET("/users/:username/repositories/:token", controllers2.ListRepos)
+	router.GET("/repositories/:username/:repoName/pull-requests", controllers2.PullRequests)
+	router.GET("/", controllers2.Index)
 }
